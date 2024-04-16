322ca5743062709554e10fe7491eca2e40799f73
Erin Y Kim <erinykim04@gmail.com>
2024-04-15 20:27:11 -0700

    Co-authored-by: Vanessa Kasminoff <VanessaKasminoff@users.noreply.github.com>

--- src/components/DisplayCard.jsx
+++ src/components/DisplayCard.jsx
@@ -1,12 +1,31 @@
-import React from "react";
-
+import React, {useState, useEffect} from "react";
+import {Loading} from '../components/Loading.jsx'
 
 export const DisplayCard = ({searchResult}) => {
     
+    //LOADING SCREEN
+  const [loading, setLoading] = useState(false);
+
+  //Will trigger loading on first render of the page.
+  useEffect(() => {
+    setLoading(true)
+    // IF USING API, could use fetch here and when getting a response from the server would remove the loading.
+    // For testing/demonstration purposes, manually setting a load time using setTimeout.
+    setTimeout(() => {
+      setLoading(false)
+    }, 5000)
+  }, [])
+
     return (
         <div className="blue-container">
-            {searchResult && <h1>{searchResult}</h1>}
-            <img src='https://placehold.co/600x400' alt='placeholder'/>
+            {loading ? (
+              <Loading loading={loading}/>
+            ) : (
+            <>
+             <h1>{searchResult}</h1>
+            <img className="placeholder-img" src='https://placehold.co/600x400' alt='placeholder'/>
+            </>
+        )}
         </div>
     )
 }
--- src/components/LeftSideButton.jsx
+++ src/components/LeftSideButton.jsx
@@ -4,7 +4,8 @@ export const LeftSideButton = () => {
     return (
         <div className='left-side-button-container'>
             <button className='select-button'>Select</button>
-            <button className='home-button'>Home</button>
+            <button className='home-button'>Pokedex</button>
+            {/* dynamically change pokedex/rocketdex */}
             <button className='menu-button'>Menu</button>
         </div>
     )
